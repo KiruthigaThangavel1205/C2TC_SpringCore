505362b5b3deba7a726f6a86261b4175f0bb40aa
Kiruthiga1205 <velkiruthiga@gmailcom>
2025-09-01 14:17:38 +0530

    Commit...

---
+++ Project_1/src/main/java/com/tnsif/Project/Project_1/BookMain.java
@@ -0,0 +1,19 @@
+package com.tnsif.Project.Project_1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+        // Load Spring configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("book.xml");
+
+        // Get Library bean
+        Library library = (Library) context.getBean("library");
+
+        // Display books
+        library.displayBooks();
+    }
+}
+
